71c7d93d507f83af6e257420e0bd0a61a1a4072d
Ralph Bodenner <rkbodenner+github@gmail.com>
2013-06-01 11:13:18 -0700

    Interface to (almost) match Ruby's Digest::SHA2, plus broken tests

---
+++ sha256-test.rs
@@ -0,0 +1,7 @@
+mod sha256;
+
+#[test]
+fn block_length() {
+  let digest = sha256::new();
+  assert!(64 == digest.block_length());
+}
---
+++ sha256.rs
@@ -0,0 +1,35 @@
+pub struct SHA256 {
+  digest: ~str
+}
+
+trait SHA2 {
+  // Block length of the digest in bytes
+  pub fn block_length(self) -> uint;
+  // Length of the hash value of the digest in bytes
+  pub fn digest_length(self) -> uint;
+  // Reset the internal state and return self
+  pub fn reset(self) -> Self;
+  // Update the digest using a given string and return self
+  pub fn update(self, msg: &str) -> Self;
+}
+
+impl SHA2 for SHA256 {
+  pub fn block_length(self) -> uint {
+    64
+  }
+  pub fn digest_length(self) -> uint {
+    32
+  }
+  pub fn reset(self) -> SHA256 {
+    self
+  }
+  pub fn update(self, msg: &str) -> SHA256 {
+    self
+  }
+}
+
+pub fn new() -> SHA256 {
+  SHA256 {
+    digest: ~""
+  } 
+}
